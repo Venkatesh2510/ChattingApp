a16303ccbe742fba70ff699b9d005401909add06
Venkatesh2510 <imvenky007@gmail.com>
2025-08-25 02:23:33 +0530

    Added login and register functionality

--- ChattingApp/client/src/layout/nav/nav.ts
+++ ChattingApp/client/src/layout/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 @Component({
   selector: 'app-nav',
   imports: [FormsModule],
@@ -7,8 +8,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.css'
 })
 export class Nav {
+    protected accountService = inject(AccountService);
     protected creds: any = {}
     login(){
-        console.log(this.creds);
+        this.accountService.login(this.creds).subscribe({
+            next: response => {
+                console.log(response);
+                this.creds = {};
+            },
+            error: error => {
+                console.error('There was an error during the login process:', error);
+            }
+          });
+    }
+    logout(){
+        this.accountService.logout();
     }
 }
