d45e6702dfa5733e57e8405f6bf5a7add8bc8e90
Venkatesh2510 <imvenky007@gmail.com>
2025-08-24 23:19:12 +0530

    nav component added

---
+++ ChattingAppTemp/client/src/app/app.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+
+@Component({
+  selector: 'app-root',
+  imports: [Nav],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title  = 'Chatting App';
+  protected members = signal<any>([])
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+  
+  async getMembers() {
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.error('Error in getMembers:', error);
+      throw error;
+    }
+  }
+}
