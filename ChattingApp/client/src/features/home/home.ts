8939cc0de0b9abd2013e23aa4b7e2b301189bb54
Venkatesh2510 <imvenky007@gmail.com>
2025-08-27 01:45:21 +0530

    Added Error handling functionality

--- ChattingApp/client/src/features/home/home.ts
+++ ChattingApp/client/src/features/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, Input, signal } from '@angular/core';
 import { Register } from "../account/register/register";
-import { User } from '../../type/user';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-home',
