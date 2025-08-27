d391e7fbe27aa44e6e42ce26f874c26f3ff4d998
Venkatesh2510 <imvenky007@gmail.com>
2025-08-27 16:50:01 +0530

    Added Repository and made DB related changes

--- ChattingApp/API/Data/AppDbContext.cs
+++ ChattingApp/API/Data/AppDbContext.cs
@@ -6,6 +6,8 @@ namespace API.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
 }
